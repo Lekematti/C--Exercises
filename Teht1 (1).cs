27abbdec07b4f99b37ace678b1bd0e5cf37c838b
Lekematti <103430948+Lekematti@users.noreply.github.com>
2023-01-28 09:45:10 +0200

    Add files via upload

---
+++ Teht1 (1).cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3
+{
+    internal class Teht1
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i < 1001)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+        }
+    }
+}
+
+//Kirjoita ohjelma, joka tulostaa kolmella jaolliset luvut väliltä 1..1000. Käytä whilesilmukkaa.
