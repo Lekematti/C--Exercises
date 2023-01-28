9e528344297cbd7e68cf6b58bcd23fde04e75133
Lekematti <103430948+Lekematti@users.noreply.github.com>
2023-01-28 09:46:19 +0200

    Add files via upload

---
+++ Teht6 (1).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht6
+{
+    internal class Teht6
+    {
+        static void Main(string[] args)
+        {
+            
+            var num = new string[10];
+            
+            for (var i = 0; i < num.Length; i++)
+            {
+                Console.Write("Anna kymmen lukua: ");
+                num[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Luvut käännetty:");
+            Array.Reverse(num);
+            foreach (var numerot in num)
+                Console.WriteLine(numerot);
+        }
+    }
+}
+/*Kirjoita ohjelma, joka kysyy käyttäjältä kymmenen lukua ja tallettaa ne taulukkoon.
+Tämän jälkeen ohjelma tulostaa luvut päinvastaisessa järjestyksessä.
+Käytä forsilmukkaa ja taulukkoa.*/
