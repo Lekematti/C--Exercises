8971c610af9a3bac6cbb923f943245252377f49d
Lekematti <103430948+Lekematti@users.noreply.github.com>
2023-01-28 09:46:00 +0200

    Add files via upload

---
+++ Teht5 (1).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht5
+{
+    internal class Teht5
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kuinka monta kaveria: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            var nim = new string[num];
+
+            for (var i = 0; i < nim.Length; i++)
+            {
+                Console.Write("Kaverin nimi: ");
+                nim[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Kaverisi ovat:");
+
+            foreach (var kaveri in nim)
+                Console.WriteLine(kaveri);
+        }
+    }
+}
+//Kysy käyttäjältä kuinka monta ystävää hänellä on. Kysy kaikkien ystävien nimet ja
+//talleta ne taulukkoon. Lopuksi tulosta ystävälista taulukosta. Käytä for-silmukkaa
+//ja taulukkoa.
