62e8b1703dbe5bdc832d61230110595582af9f1f
Lekematti <103430948+Lekematti@users.noreply.github.com>
2023-01-28 09:34:28 +0200

    Create Ex3.cs

---
+++ Ex3.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht6
+{
+    internal class teht6
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Illallisien kalorimäärä");
+            int cal = Convert.ToInt32(Console.ReadLine());
+            double joul = 4.1840;
+            double tulos = cal * joul;
+            Console.WriteLine(cal + " kalorin illallinen on " + tulos + " joulea");
+        }
+    }
+}
