35fb5a419e694420f119aa00a3e1eb7338b0863f
Lekematti <103430948+Lekematti@users.noreply.github.com>
2023-01-28 09:46:27 +0200

    Add files via upload

---
+++ Teht7.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht7
+{
+    internal class Teht7
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kuinka monta tähteä (*) : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            var nim = new string[num];
+
+            for (var i = 0; i < nim.Length; i++)
+            {
+                nim[i] = "*";
+            }
+            Console.WriteLine(num + " tähteä:");
+
+            foreach (var tahti in nim)
+                Console.Write(tahti);
+                Console.WriteLine();
+        }
+    }
+}
+/*Kysy käyttäjältä kuinka monta tähteä (*) tulostetaan. Tämän jälkeen ohjelma
+tulostaa tämänmittaisen tähtirivin, esim. 7 tähteä:
+********
+Käytä for-silmukkaa.
+ */
